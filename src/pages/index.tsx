2b14f6d7beca7c4e2cef596de2493fc01f002b5a
Tim Krause <timkrausedev@gmail.com>
2024-04-01 18:37:25 -0400

    update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,63 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+
+import GridWrapper from '@site/src/providers/GridProvider';
+import LinkCardGroup from '@site/src/components/LinkCardGroup';
+import Spacer from '@site/src/components/Spacer';
+import { PropType as LinkProps } from '@site/src/components/LinkCard';
 
 import styles from './index.module.scss';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+const linkItems: LinkProps[] = [
+  {
+    title: 'Recipes',
+    Svg: require('@site/static/img/home-plants.svg').default,
+    linkTo: '/docs/recipes/intro',
+  },
+  {
+    title: 'Plants',
+    Svg: require('@site/static/img/home-recipes.svg').default,
+    linkTo: '/docs/plants/current',
+  },
+  {
+    title: 'Misc',
+    Svg: require('@site/static/img/home-misc.svg').default,
+    linkTo: '/docs/misc/intro',
+  },
+]
+
+const HomepageHero = (): JSX.Element => {
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+    <div className="container">
+        <div className={styles.heroBanner}>
+          <Spacer height={2} />
+          <h1>Welcome!</h1>
+          <p className='lead'>This is Tim's Wiki of Information</p>
+          <Spacer height={2} />
         </div>
-      </div>
-    </header>
+    </div>
+  );
+}
+
+const HomepageFeatures = (): JSX.Element => {
+  return (
+    <div className="container">
+      <LinkCardGroup linkItems={linkItems} />
+    </div>
   );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+    <GridWrapper>
+      <Layout title="Home">
+        <main>
+          <Spacer height={4} />
+          <HomepageHero />
+          <Spacer height={4} />
+          <HomepageFeatures />
+          <Spacer height={4} />
+        </main>
+      </Layout>
+    </GridWrapper>
   );
 }
